a2620a2dc2ce502c0ff0d92f96f4dbdd09366b33
almandin <almandin-jv@hotmail.fr>
2014-12-09 15:31:37 +0000

    pb

--- code/utile/mouvement.py
+++
@@ -1,6 +0,0 @@
-class Mouvement :
-    def __init__(self, destination, nbUnites, duree, joueur) :
-        self.destination = destination
-        self.nbUnites = nbUnites
-        self.duree = duree
-        self.joueur = joueur
