e4a8f76086668442bde0e07663778e377cce9a1d
almandin <almandin-jv@hotmail.fr>
2014-12-10 21:34:17 +0100

    correction d'un bug

--- code/ragnar.py
+++ code/ragnar.py
@@ -46,5 +46,5 @@ def play_pooo():
 
 
 register_pooo("0947e717-02a1-4d83-9470-a941b6e8ed07")
-init_pooo("INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;2CELLS:1[2]23'9'2'30'8'1,2[1]41'55'1'30'8'2;2LINES:1@3433OF2,1@6502OF3")
+init_pooo("INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3")
 play_pooo()
