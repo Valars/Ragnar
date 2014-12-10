64cfa16c5ccc45ecda65991291b33f3bb2930cc4
almandin <almandin-jv@hotmail.fr>
2014-12-10 21:53:16 +0100

    modifs

--- code/majObjets.py
+++ code/majObjets.py
@@ -2,10 +2,8 @@ from partie import *
 #fonction mettant à jour le plateau en fonction du résultat de la fonction parseState()
 def majPlateau(changements, plateau) :
 
-    noeuds = changements["noeuds"]
+    noeudsDyn = changements["noeuds"]
     moves = changements["moves"]
-    print("moves : ")
-    for move in moves :
-        print(move)
 
-    print(plateau)
+    for ligne in plateau["lignes"] :
+        ligne.printArete()
