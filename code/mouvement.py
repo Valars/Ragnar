cdc8ab94e4c3d3e951dc498cde80e298bac4bfbc
almandin <almandin-jv@hotmail.fr>
2014-12-10 22:29:38 +0100

    changements mineurs

--- code/mouvement.py
+++ code/mouvement.py
@@ -1,10 +1,10 @@
 '''Nom : Mouvement
 
-Attributs : destination : Noeud : Noeud de destination 
+Attributs : destination : Noeud : Noeud de destination
             nbr         : Int   : Nombre d'unité offensive en cours de déplacement pour ce mouvement
             impact      : Int   : Temps restant avant arrivée à destination (en ms)
             proprio     : Int   : Joueur proprietaire des unités (cf. Partie.player)
-            
+
 '''
 class Mouvement :
     def __init__(self, destination, nbUnites, duree, joueur) :
