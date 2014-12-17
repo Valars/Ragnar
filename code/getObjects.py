2163721d90652f75675f3f0426134b08505fe7b0
almandin <almandin-jv@hotmail.fr>
2014-12-17 11:13:06 +0000

    Commentage

--- code/getObjects.py
+++ code/getObjects.py
@@ -1,15 +1,22 @@
 '''Nom : getArete()
 
-E : partie : Partie : Instance de la partie en cours
-    idNoeud1 : Noeud : Identifiant de l'un des noeud extremité de l'arete souhaité
-    idNoeud2 : Noeud : Identifiant du second noeud extremité de l'arete souhaité
+E : partie   : Partie : Instance de la partie en cours
+    idNoeud1 : Noeud  : Identifiant de l'un des noeud extremité de l'arete souhaité
+    idNoeud2 : Noeud  : Identifiant du second noeud extremité de l'arete souhaité
     
-S :          : Arete : réference vers l'arete souhaité
-
+S :          : Arete  : réference vers l'arete souhaité
 '''
 
 def getArete(partie, idNoeud1, idNoeud2):
     return partie.plateau["lignes"][str(idNoeud1)+";"+str(idNoeud2)]
+
+'''Nom : getNoeud()
+
+E : partie : Partie : Instance de la partie en cours
+    id     : Int    : Id du Noeud souhaité
     
+S :          : Arete  : réference vers le noeud souhaité
+'''
+
 def getNoeud(partie, id) :
     return partie.plateau["noeuds"][str(id)]
