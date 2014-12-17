0f6464b7acf272c1e3e484761e9d1401c941cb4e
almandin <almandin-jv@hotmail.fr>
2014-12-17 18:38:39 +0100

    parfaitement fonctinnel !

--- code/plusCourtChemin.py
+++ code/plusCourtChemin.py
@@ -1,2 +1,28 @@
-#retourne la liste des aretes qu'il faut prendre pour aller de noeud1 à noeud2 le plus vite
-def plusCourtChemin(partie, noeud1, noeud2) :
+#retourne la liste des noeuds qu'il faut parcourir pour aller du noeud from au noeud to
+# from : noeud
+#to : noeud
+#retourne : liste de noeuds (plus court chemin entre from et to)
+def plusCourtChemin(de, vers) :
+    parcouru = []
+    possibles = [de]
+    courant = de
+    fin = False
+
+    while courant != vers and fin != True :
+
+        for aretevoisine in courant.aretesConnectees :
+            if aretevoisine.noeud1 == courant :
+                levoisin = aretevoisine.noeud2
+            else :
+                levoisin = aretevoisine.noeud1
+            if levoisin == vers :
+                parcouru.append(courant)
+                parcouru.append(levoisin)
+                fin = True
+            else :
+                possibles.append(levoisin)
+        if fin == False :
+            parcouru.append(courant)
+            possibles.pop(0)
+        courant = possibles[0]
+    return parcouru
