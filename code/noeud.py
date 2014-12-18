cdb7b469bbad742b26feed9a808950cab4973bf3
almandin <almandin-jv@hotmail.fr>
2014-12-18 16:19:46 +0000

    modif

--- code/noeud.py
+++ code/noeud.py
@@ -1,20 +1,21 @@
 '''Nom : Noeud
 
-Attributs : id          : Int   : identifiant (unique pour la partie) le noeud
-            proprio     : Int   : identifiant de partie(cf. Partie.player) du joueur propriétaire du noeud
-            x           : Int   : coordonnée en abscisse du noeud (pour l'affichage graphique)
-            y           : Int   : coordonnée en ordonné du noeud (pour l'affichage graphique)
-            radius      : Int   : diametre du noeud (pour l'affichage graphique)
-            offsize     : Int   : nombre maximal d'unités offensives que la cellule peut posséder
-            defsize     : Int   : nombre maximal d'unités défensives que la cellule peut posséder  
-            prod        : Float : production d'unité en unité/sec 
-            effectifOff : Int   : nombre d'unités offensives possedées
-            effectifDef : Int   : nombre d'unités defensives possedées
-            listeArete  : Liste : contient les objets Arete connectés au noeud
+Attributs : id          : Int                   : identifiant (unique pour la partie) le noeud
+            proprio     : Int                   : identifiant de partie(cf. Partie.player) du joueur propriétaire du noeud
+            x           : Int                   : coordonnée en abscisse du noeud (pour l'affichage graphique)
+            y           : Int                   : coordonnée en ordonné du noeud (pour l'affichage graphique)
+            radius      : Int                   : diametre du noeud (pour l'affichage graphique)
+            offsize     : Int                   : nombre maximal d'unités offensives que la cellule peut posséder
+            defsize     : Int                   : nombre maximal d'unités défensives que la cellule peut posséder  
+            prod        : Float                 : production d'unité en unité/sec 
+            effectifOff : Int                   : nombre d'unités offensives possedées
+            effectifDef : Int                   : nombre d'unités defensives possedées
+            listeArete  : Liste                 : contient les objets Arete connectés au noeud
+            role        : Chaine de caractères  : Role du noeud, si il nous appartient, Fournisseur ou Rush ou Attaque
 '''
 
 class Noeud :
-    def __init__(self, id, x, y, radius, offsize, defsize, prod, proprio=-1,off=0,defenses=0,aretesConnectees=[]) :
+    def __init__(self, id, x, y, radius, offsize, defsize, prod, proprio=-1,off=0,defenses=0,aretesConnectees=[], role = "") :
         self.id = id
         self.proprio = proprio
         self.x = x
@@ -26,6 +27,7 @@ class Noeud :
         self.off = off
         self.defenses = defenses
         self.aretesConnectees = aretesConnectees
+        self.role = role
 
     def printNoeud(self) :
         print("Noeud "+str(self.id))
