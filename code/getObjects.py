9788ea001c9702ca1848b09ca9032beb89afd955
almandin <almandin-jv@hotmail.fr>
2015-01-08 16:48:43 +0000

    qmldklm

--- code/getObjects.py
+++ code/getObjects.py
@@ -1,30 +1,31 @@
-'''Nom : getArete()
+'''Nom : getArete(partie, idnoeud1, idnoeud2)
 
-E : partie   : Partie : Instance de la partie en cours
-    idNoeud1 : Noeud  : Identifiant de l'un des noeud extremité de l'arete souhaité
-    idNoeud2 : Noeud  : Identifiant du second noeud extremité de l'arete souhaité
-
-S :          : Arete  : réference vers l'arete souhaité
+    E : partie   : Partie : Instance de la partie en cours
+        idNoeud1 : Int   : Identifiant de l'un des noeud extremité de l'arete souhaité
+        idNoeud2 : Int   : Identifiant du second noeud extremité de l'arete souhaité
+    
+    S :          : Arete  : réference vers l'arete souhaité
 '''
 
 def getArete(partie, idNoeud1, idNoeud2):
     return partie.plateau["lignes"][str(idNoeud1)+";"+str(idNoeud2)]
 
-'''Nom : getNoeud()
+'''Nom : getNoeud(partie, idnoeud)
 
-E : partie : Partie : Instance de la partie en cours
-    id     : Int    : Id du Noeud souhaité
-
-S :        : Arete  : réference vers le noeud souhaité
+    E : partie  : Partie : Instance de la partie en cours
+        idnoeud : Int    : Id du Noeud souhaité
+    
+    S :         : Arete  : réference vers le noeud souhaité
 '''
 
-def getNoeud(partie, id) :
-    return partie.plateau["noeuds"][str(id)]
+def getNoeud(partie, idnoeud) :
+    return partie.plateau["noeuds"][str(idnoeud)]
 
-''' Nom : getVoisins()
-E : noeud : objet Noeud dont on veut obtenir les noeuds voisins
+''' Nom : getVoisins(noeud)
 
-S : Liste : liste d'objets Noeud, qui sont les voisins de l'entrée noeud
+    E : Noeud : objet Noeud dont on veut obtenir les noeuds voisins
+    
+    S : Liste : liste d'objets Noeud, qui sont les voisins de l'entrée noeud
 '''
 def getVoisins(noeud) :
     voisins = []
@@ -35,5 +36,12 @@ def getVoisins(noeud) :
             voisins.append(ligne.noeud1)
     return voisins
 
+''' Nom : getDistance(noeud1, noeud2)
+
+    E : noeud1 : Noeud : la premiere extremité du chemin dont on veut la distance
+        noeud2 : Noeud : La seconde extremité du chemin dont on veut la distance
+    
+    S :          Int   : La distance du chemin
+'''
 def getDistance(noeud1, noeud2) :
     return noeud1.distances[str(noeud2.id)]
