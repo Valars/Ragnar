f4dfc099b7ebad317e4b5fc136a9b2c77eeb63cf
almandin <almandin-jv@hotmail.fr>
2015-01-08 16:49:37 +0000

    ajout du fichier des fonctions qui seront utiles pour les IA

---
+++ code/fonctions_utiles.py
@@ -0,0 +1,12 @@
+def peutEnvoyer(noeud):
+    
+    pass
+
+def doitEnvoyer(noeud_attaquant, noeud_defenseur):
+    
+    pass
+
+def peutCapturer(noeud_attaquant, noeud_defenseur):
+    
+    pass
+
