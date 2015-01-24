6b1f2c95ca51b6217d38f8d6ad1acaddf80df3ba
virgile <almandin-jv@hotmail.fr>
2015-01-24 19:13:48 +0100

    woop woop ca marche !

--- code/IAAttaquants.py
+++ code/IAAttaquants.py
@@ -11,26 +11,12 @@ def IAAttaquants(partie, mesNoeuds, cellsCapturees) :
 		#faire jouer tous nos attaquants
 		#tri des cellules qui vont se faire capturées par la distance à attaquant
 		cellulesASauver = list(cellsCapturees)
-		cellulesASauver = sorted(cellulesASauver, key=lambda dist: dist[0].distances[str(attaquant.id)][1])
-		#essayer de sauver un noeud parmis cellsCapturees
-		#regarder lequel (faire une rentabilité distance/cout en unités)
-		pasEnvoye = False
-		for cell in cellulesASauver :
-			if cell[0] != attaquant :
-				#c'est dans l'ordre du plus proche au plus éloigné de attaquant
-				if attaquant.off > cell[1] :
-					units = ceil((cell[1]+1)/(attaquant.off)*100) #on envoi ce qu'il faut + 1 unités pour sauver
-					mouv(partie, attaquant, getNoeud(partie,attaquant.distances[str(cell[0].id)][0][0]), units)
-					pasEnvoye = False
+		if len(cellulesASauver) > 0 :
+			cellulesASauver = sorted(cellulesASauver, key=lambda dist: dist[0].distances[str(attaquant.id)][1])
+			for cell in cellulesASauver :
+				if attaquant != cell[0] :
+					mouv(partie, attaquant, getNoeud(partie,attaquant.distances[str(cell[0].id)][0][0]),50)
 					break
-				pasEnvoye = True
-		#on a essayé d'envoyer des unités vers une unité à sauver
-	#si on n'avait pas assez d'unités, on en envoi les deux tiers de ce qu'on a pour renfort
-		if pasEnvoye : #si on pas eu assez d'unités pour pouvoir sauver une unité
-			cible = getNoeud(partie, attaquant.distances[str(cellulesASauver[0][0].id)][0][0])
-		else :
-			for voisin in voisins :
-				if voisin.proprio != -1 and voisin.proprio != attaquant.proprio :
-					cible = voisin
-					break
-		mouv(partie, attaquant, getNoeud(partie,attaquant.distances[str(cible.id)][0][0]), 100)
+		for voisin in voisins :
+			if voisin.proprio != -1 and voisin.proprio != attaquant.proprio :
+				mouv(partie, attaquant, voisin,100)
